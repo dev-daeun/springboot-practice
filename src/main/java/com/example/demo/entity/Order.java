93da9d231b52cf1644082acc3216e8d2839da4bc
dev-daeun <devdaeun@gmail.com>
2024-06-18 19:25:28 +0900

    Order 엔티티 정의

---
+++ src/main/java/com/example/demo/entity/Order.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "zone_id", nullable = false)
+    private int zoneId;
+
+    @Column(name = "delivery_vendor")
+    private String deliveryVendor;
+
+    @Column(name = "order_number", nullable = false, unique = true)
+    private String orderNumber;
+
+}
