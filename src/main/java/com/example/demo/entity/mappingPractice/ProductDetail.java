a8eb6bd1685137d574e8c60fcfac5339bec11f8f
dev-daeun <devdaeun@gmail.com>
2024-06-27 10:58:09 +0900

    마이그레이션 결과를 주석으로 작성

--- src/main/java/com/example/demo/entity/mappingPractice/ProductDetail.java
+++ src/main/java/com/example/demo/entity/mappingPractice/ProductDetail.java
@@ -24,3 +24,22 @@ public class ProductDetail extends BaseEntity {
     @JoinColumn(name = "product_id")
     private Product product;
 }
+
+//mysql> show columns from product_detail;
+//        +-------------+--------------+------+-----+---------+----------------+
+//        | Field       | Type         | Null | Key | Default | Extra          |
+//        +-------------+--------------+------+-----+---------+----------------+
+//        | id          | bigint       | NO   | PRI | NULL    | auto_increment |
+//        | created_at  | datetime(6)  | YES  |     | NULL    |                |
+//        | updated_at  | datetime(6)  | YES  |     | NULL    |                |
+//        | description | varchar(255) | YES  |     | NULL    |                |
+//        | product_id  | bigint       | YES  | UNI | NULL    |                |
+//        +-------------+--------------+------+-----+---------+----------------+
+//        5 rows in set (0.03 sec)
+//mysql> show index from product_detail;
+//+----------------+------------+-----------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
+//| Table          | Non_unique | Key_name                    | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment | Visible | Expression |
+//+----------------+------------+-----------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
+//| product_detail |          0 | PRIMARY                     |            1 | id          | A         |           0 |     NULL |   NULL |      | BTREE      |         |               | YES     | NULL       |
+//| product_detail |          0 | UKn0poneags4pq2e3u2mlv7h29n |            1 | product_id  | A         |           0 |     NULL |   NULL | YES  | BTREE      |         |               | YES     | NULL       |
+//+----------------+------------+-----------------------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+---------+------------+
